d28f1323dd60c6bd1ff29b486f38a90571bf8b97
Bhuvan Mohanajothy <44739093+aerobhuvan@users.noreply.github.com>
2024-02-15 11:13:47 -0500

    Update streamlit_app.py
    edited index

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ streamlit.text('Bhuvi text');
 streamlit.text('Bhuvi text2');
 
 my_pandas_df = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
+my_pandas_df=my_pandas_df.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_pandas_df.index))
 streamlit.dataframe(my_pandas_df);
