5530fb8b24851ae41f454bb9badb49c2aab65a0b
Bhuvan Mohanajothy <44739093+aerobhuvan@users.noreply.github.com>
2024-02-15 11:05:00 -0500

    Update streamlit_app.py
    Read from s3 using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('Bhuvi First app');
 streamlit.header('Bhuvi header');
 streamlit.text('Bhuvi text');
 streamlit.text('Bhuvi text2');
+
+my_pandas_df = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
+streamlit.dataframe(my_pandas_df);
