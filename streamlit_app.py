60dd730742000299df9c887fbe94217848ee99c1
Bhuvan Mohanajothy <44739093+aerobhuvan@users.noreply.github.com>
2024-02-16 17:33:30 -0500

    Update streamlit_app.py
    Adding response from fruityvice API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,8 @@ my_pandas_df=my_pandas_df.set_index('Fruit');
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_pandas_df.index), ["Avocado","Strawberries"]);
 fruit_to_show = my_pandas_df.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show);
+
+#New section to display fruitviceapi
+import requests
+fruityvise_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvise_response)
